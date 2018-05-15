a6a948077961ad607a6d071621e897e91ad1f420
Philippe Raemy <philippe_raemy@swissonline.ch>
2018-05-15 17:06:00 +0200

    Use using for SqlConnection

--- SqlObjetsRecompiler/Program.cs
+++ SqlObjetsRecompiler/Program.cs
@@ -18,58 +18,60 @@ namespace SqlObjetsRecompiler
             var databaseName = "master";
             args.GetArgumentValue("-S", v => serverName = v)
                 .GetArgumentValue("-d", v => databaseName = v);
-            var server =
-                new Server(new ServerConnection(
-                    new SqlConnection($"server={serverName};database={databaseName};trusted_connection=true;")));
-            var scriptingOptions = new ScriptingOptions
+            using (var conn = new SqlConnection($"server={serverName};database={databaseName};trusted_connection=true;"))
             {
-                AnsiFile = true,
-                ClusteredIndexes = true,
-                ContinueScriptingOnError = false,
-                DdlBodyOnly = false,
-                DdlHeaderOnly = false,
-                DriAll = true,
-                EnforceScriptingOptions = true,
-                FullTextIndexes = true,
-                IncludeDatabaseContext = false,
-                IncludeIfNotExists = false,
-                Indexes = true,
-                Permissions = false,
-                SchemaQualify = true,
-                SchemaQualifyForeignKeysReferences = true,
-                ScriptData = false,
-                ScriptSchema = true,
-                Triggers = true,
-                ScriptBatchTerminator = true 
-            };
+                var server = new Server(new ServerConnection(conn));
+                var scriptingOptions = new ScriptingOptions
+                {
+                    AnsiFile                           = true,
+                    ClusteredIndexes                   = true,
+                    ContinueScriptingOnError           = false,
+                    DdlBodyOnly                        = false,
+                    DdlHeaderOnly                      = false,
+                    DriAll                             = true,
+                    EnforceScriptingOptions            = true,
+                    FullTextIndexes                    = true,
+                    IncludeDatabaseContext             = false,
+                    IncludeIfNotExists                 = false,
+                    Indexes                            = true,
+                    Permissions                        = false,
+                    SchemaQualify                      = true,
+                    SchemaQualifyForeignKeysReferences = true,
+                    ScriptData                         = false,
+                    ScriptSchema                       = true,
+                    Triggers                           = true,
+                    ScriptBatchTerminator              = true
+                };
 
-            var db = server.Databases[databaseName];
-            Console.WriteLine(db.Name);
-            Console.WriteLine(new string('#', db.Name.Length));
-            (from table in db.Tables.Cast<Table>()
-                    where !table.IsSystemObject
-                    from trigger in table.Triggers.Cast<Trigger>()
-                    select (Table: table, Trigger: trigger)
-                ).ForEach(t => ScriptObject(db, t.Trigger.Script(scriptingOptions),
-                    $"Trigger {t.Table.Schema}.{t.Table.Name}.{t.Trigger.Name}", @"CREATE\s+TRIGGER", "ALTER TRIGGER"));
+                var db = server.Databases[databaseName];
+                Console.WriteLine(db.Name);
+                Console.WriteLine(new string('#', db.Name.Length));
+                (from table in db.Tables.Cast<Table>()
+                        where !table.IsSystemObject
+                        from trigger in table.Triggers.Cast<Trigger>()
+                        select (Table: table, Trigger: trigger)
+                    ).ForEach(t => ScriptObject(db, t.Trigger.Script(scriptingOptions),
+                        $"Trigger {t.Table.Schema}.{t.Table.Name}.{t.Trigger.Name}", @"CREATE\s+TRIGGER",
+                        "ALTER TRIGGER"));
 
-            (from s in db.StoredProcedures.Cast<StoredProcedure>()
-                    where !s.IsSystemObject
-                    select s
-                ).ForEach(s => ScriptObject(db, s.Script(scriptingOptions), $"Stored proc {s.Schema}.{s.Name}",
-                    @"CREATE\s+PROCEDURE", "ALTER PROCEDURE"));
+                (from s in db.StoredProcedures.Cast<StoredProcedure>()
+                        where !s.IsSystemObject
+                        select s
+                    ).ForEach(s => ScriptObject(db, s.Script(scriptingOptions), $"Stored proc {s.Schema}.{s.Name}",
+                        @"CREATE\s+PROCEDURE", "ALTER PROCEDURE"));
 
-            (from f in db.UserDefinedFunctions.Cast<UserDefinedFunction>()
-                    where !f.IsSystemObject
-                    select f
-                ).ForEach(f => ScriptObject(db, f.Script(scriptingOptions), $"Function {f.Schema}.{f.Name}",
-                    @"CREATE\s+FUNCTION", "ALTER FUNCTION"));
+                (from f in db.UserDefinedFunctions.Cast<UserDefinedFunction>()
+                        where !f.IsSystemObject
+                        select f
+                    ).ForEach(f => ScriptObject(db, f.Script(scriptingOptions), $"Function {f.Schema}.{f.Name}",
+                        @"CREATE\s+FUNCTION", "ALTER FUNCTION"));
 
-            (from v in db.Views.Cast<View>()
-                    where !v.IsSystemObject
-                    select v
-                ).ForEach(v => ScriptObject(db, v.Script(scriptingOptions), $"View {v.Schema}.{v.Name}",
-                    @"CREATE\s+VIEW", "ALTER VIEW"));
+                (from v in db.Views.Cast<View>()
+                        where !v.IsSystemObject
+                        select v
+                    ).ForEach(v => ScriptObject(db, v.Script(scriptingOptions), $"View {v.Schema}.{v.Name}",
+                        @"CREATE\s+VIEW", "ALTER VIEW"));
+            }
         }
 
         static void ScriptObject(Database db, StringCollection script, string name, string createStatement, string alterStatement)
@@ -85,9 +87,6 @@ namespace SqlObjetsRecompiler
             );
             try
             {
-                // var cmd = new SqlCommand(sql, server.ConnectionContext.SqlConnectionObject);
-                // cmd.Transaction = tran;
-                // cmd.ExecuteNonQuery();
                 db.ExecuteNonQuery(sql);
                 Console.WriteLine($"Recompiled {name}.");
             }
