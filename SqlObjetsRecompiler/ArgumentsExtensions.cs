6386938fbc3487b1a1bc496269968838006ecc90
Philippe Raemy <philippe_raemy@swissonline.ch>
2018-05-15 16:54:58 +0200

    Initial commit

---
+++ SqlObjetsRecompiler/ArgumentsExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace SqlObjetsRecompiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    static class ArgumentsExtensions
+    {
+        public static IEnumerable<string> GetArgumentValue(this IEnumerable<string> args, string arg, Action<string> valueSetter)
+            => args.GetArgumentValue(arg, valueSetter, null);
+
+        public static IEnumerable<string> GetSwitchValue(this IEnumerable<string> args, string arg, Action switchSetter)
+            => args.GetArgumentValue(arg, null, switchSetter);
+
+        public static IEnumerable<string> GetArgumentValue(this IEnumerable<string> args, string arg, Action<string> valueSetter, Action switchSetter)
+        {
+            var argArray = args as string[] ?? args.ToArray();
+            if (valueSetter == null && switchSetter != null)
+            {
+                if (argArray.Any(a => string.Equals(a, arg, StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    switchSetter();
+                    return argArray
+                        .Where(a => !string.Equals(a, arg, StringComparison.InvariantCultureIgnoreCase));
+                }
+            }
+            else if (valueSetter != null)
+            {
+                var value = argArray
+                    .SkipWhile(a => !string.Equals(a, arg, StringComparison.InvariantCultureIgnoreCase))
+                    .Skip(1)
+                    .FirstOrDefault();
+                valueSetter(value);
+                if (value != null) switchSetter?.Invoke();
+                return argArray.TakeWhile(a => !string.Equals(a, arg, StringComparison.InvariantCultureIgnoreCase))
+                    .Concat(argArray.SkipWhile(a => !string.Equals(a, arg, StringComparison.InvariantCultureIgnoreCase)).Skip(2));
+            }
+
+            return argArray;
+        }
+    }
+}
---
+++ SqlObjetsRecompiler/Program.cs
@@ -0,0 +1,85 @@
+﻿
+namespace SqlObjetsRecompiler
+{
+    using System;
+    using System.Collections.Specialized;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using Microsoft.SqlServer.Management.Common;
+    using Microsoft.SqlServer.Management.Smo;
+    using MoreLinq;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serverName = ".";
+            var databaseName = "master";
+            args.GetArgumentValue("-S", v => serverName = v)
+                .GetArgumentValue("-d", v => databaseName = v);
+            var server =
+                new Server(new ServerConnection(
+                    new SqlConnection($"server={serverName};database={databaseName};trusted_connection=true;")));
+            var scriptingOptions = new ScriptingOptions
+            {
+                AnsiFile = true,
+                ClusteredIndexes = true,
+                ContinueScriptingOnError = false,
+                DdlBodyOnly = false,
+                DdlHeaderOnly = false,
+                DriAll = true,
+                EnforceScriptingOptions = true,
+                FullTextIndexes = true,
+                IncludeDatabaseContext = false,
+                IncludeIfNotExists = false,
+                Indexes = true,
+                Permissions = false,
+                SchemaQualify = true,
+                SchemaQualifyForeignKeysReferences = true,
+                ScriptData = false,
+                ScriptSchema = true,
+                Triggers = true,
+                ScriptBatchTerminator = true 
+            };
+
+            var db = server.Databases[databaseName];
+            Console.WriteLine(db.Name);
+            Console.WriteLine(new string('#', db.Name.Length));
+            foreach (Table table in db.Tables)
+                foreach (Trigger t in table.Triggers)                  ScriptObject(db, t.Script(scriptingOptions), $"Trigger {table.Schema}.{table.Name}.{t.Name}", @"CREATE\s+TRIGGER"  , "ALTER TRIGGER"  );
+            foreach (StoredProcedure s in db.StoredProcedures)         ScriptObject(db, s.Script(scriptingOptions), $"Stored proc {s.Schema}.{s.Name}",              @"CREATE\s+PROCEDURE", "ALTER PROCEDURE");
+            foreach (UserDefinedFunction f in db.UserDefinedFunctions) ScriptObject(db, f.Script(scriptingOptions), $"Function {f.Schema}.{f.Name}",                 @"CREATE\s+FUNCTION" , "ALTER FUNCTION" );
+        }
+
+        static void ScriptObject(Database db, StringCollection script, string name, string createStatement, string alterStatement)
+        {
+            var sql = new StringCollection();
+            sql.AddRange(script
+                .Cast<string>()
+                .Select(s => Regex.Replace(s, createStatement, alterStatement,
+                    RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+                .Prepend("BEGIN TRANSACTION")
+                .Concat("ROLLBACK")
+                .ToArray()
+            );
+            try
+            {
+                // var cmd = new SqlCommand(sql, server.ConnectionContext.SqlConnectionObject);
+                // cmd.Transaction = tran;
+                // cmd.ExecuteNonQuery();
+                db.ExecuteNonQuery(sql);
+                Console.WriteLine($"Recompiled {name}.");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(new string('=', name.Length));
+                Console.Error.WriteLine(name);
+                Console.Error.WriteLine(e.Message);
+                Console.Error.WriteLine(new string('=', name.Length));
+                Console.Error.WriteLine(sql);
+                Console.Error.WriteLine(new string('=', name.Length));
+            }
+        }
+    }
+}
